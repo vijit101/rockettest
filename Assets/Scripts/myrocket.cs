be18f2ade6172c15b44cb93dd473c3ae14f066dd
vijit101 <40598861+vijit101@users.noreply.github.com>
2018-07-10 23:32:57 +0530

    Again in this simple version of rocket i tried to thrust but it did show anomaly behaviour in addrelative force
    Needs help

---
+++ Assets/Scripts/myrocket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myrocket : MonoBehaviour {
+    Rigidbody myrigidbd;
+    public float rocketspeed = 1;
+
+	// Use this for initialization
+	void Start () {
+        myrigidbd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            
+            myrigidbd.AddRelativeForce(Vector3.up*rocketspeed, ForceMode.Force);
+            Debug.Log("i am happy");
+        }
+	}
+}
